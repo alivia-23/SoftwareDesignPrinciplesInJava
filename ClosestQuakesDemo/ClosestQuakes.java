a14b2837b6f486195e2d7b075d4983f2fd3d3390
alivia-23 <guin.al@husky.neu.edu>
2023-09-18 17:50:01 -0700

    closest quakes

--- ClosestQuakesDemo/ClosestQuakes.java
+++ ClosestQuakesDemo/ClosestQuakes.java
@@ -2,7 +2,7 @@
 /**
  * Find N-closest quakes
  * 
- * @author Duke Software/Learn to Program
+ * @author Alivia Guin
  * @version 1.0, November 2015
  */
 
@@ -32,19 +32,19 @@ public class ClosestQuakes
     
    public void findClosestQuakes(){
        EarthQuakeParser parser = new EarthQuakeParser();
-		//String source = "data/nov20quakedata.atom";
-		String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
-		ArrayList<QuakeEntry> list  = parser.read(source);
-		System.out.println("read data for " + list.size());
-		
-		Location jakarta  = new Location(-6.211, 106.845);
+        String source = "data/nov20quakedatasmall.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        System.out.println("read data for " + list.size());
+        
+        Location jakarta  = new Location(-6.211, 106.845);
 
-		ArrayList<QuakeEntry> close = getClosest(list, jakarta, 10);
-		for(int k=0; k < close.size(); k++){
-			QuakeEntry entry = close.get(k);
-			double distanceInMeters = jakarta.distanceTo(entry.getLocation());
-			System.out.printf("%4.2f\t %s\n", distanceInMeters/1000, entry);
-		}
-		System.out.println("number found: " + close.size());
+        ArrayList<QuakeEntry> close = getClosest(list, jakarta, 3);
+        for(int k=0; k < close.size(); k++){
+            QuakeEntry entry = close.get(k);
+            double distanceInMeters = jakarta.distanceTo(entry.getLocation());
+            System.out.printf("%4.2f\t %s\n", distanceInMeters/1000, entry);
+        }
+        System.out.println("number found: " + close.size());
    }
 }
---
+++ ClosestQuakesDemo/LargestQuakes.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class LargestQuakes here.
+ *
+ * @author (Alivia Guin)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+import edu.duke.*;
+
+public class LargestQuakes
+{
+    
+}
